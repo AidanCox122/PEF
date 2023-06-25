7ec6ef470ee3e64ac50317a5db72ddb3ade9c870
AidanCox122 <aidancox122@gmail.com>
2023-06-24 20:01:30 -0700

    formatted training data for daily habitat models and added get_weight function for logit models

--- code/analysis/fineScale_models.R
+++ code/analysis/fineScale_models.R
@@ -15,3 +15,74 @@ mbm_data <-
   read_csv('data/clean/mbm_master.csv') %>% 
   rename(zone = Zone)
 
+dth <- 
+  read_csv('data/clean/dth.csv')
+
+cruises_ocean_time_all <- data.frame(
+  Date = c("2017-10-03", "2017-10-10", "2017-10-24", "2017-10-31", "2017-11-07", "2017-11-16", "2018-10-02", "2018-10-11", "2018-10-16", "2018-10-23", "2018-10-30", "2018-11-06", "2019-10-02", "2019-10-19", "2019-10-23", "2019-10-30", "2019-11-05", "2020-10-06", "2020-10-07", "2020-10-15", "2020-10-20", "2020-10-26", "2020-11-02", "2020-11-10", "2021-10-07", "2021-10-12", "2021-10-19", "2021-10-26"),
+  ocean_time = c(276, 283, 297, 304, 311, 320, 640, 649, 654, 661, 668, 675, 1005, 1022, 1026, 1033, 1034, 1375, 1376, 1384, 1389, 1395, 1402, 1410, 1741, 1746, 1753, 1760),
+  cruise = c(seq(1,28))) %>% 
+  mutate(Date = lubridate::date(Date),
+         year = lubridate::year(Date),
+         cruise.gen = 1) %>% 
+  group_by(year) %>% 
+  mutate(cruise.gen = cumsum(cruise.gen)) %>% 
+  ungroup()
+
+# format training data ----------------------------------------------------
+
+# add environmental data to each mbm obs. 
+daily_mbm_grid <- 
+  env_grid %>% 
+  # add delta-tide-height variable
+  left_join(dth, by = 'Date') %>% 
+  # remove grid cells that do not fall witin zones (cannot be used to train)
+  filter(!is.na(zone)) %>% 
+  # change zone to a factor
+  mutate(zone = factor(zone)) %>% 
+  # select variables for model
+  dplyr::select(Date, zone, bathy:salt,dth) %>% 
+  # scale predictors
+  mutate_if(is.numeric, base::scale) %>% 
+  # find average value in each zone on each cruise date
+  group_by(Date, zone) %>% 
+  # take the mean of environmental conditions in each zone on each cruise
+  summarize_if(is.numeric, mean, na.rm = T) %>%  # n = 1644
+  ungroup() %>% 
+  right_join(
+    mbm_data %>%
+      # change zone to factor in mbm_data to match env_grid
+      mutate(zone = factor(zone)),
+    by = c('Date', 'zone')) %>%
+  # lots of NAs from years prior to 2017
+  filter(!is.na(bathy)) %>% # n = 672
+  mutate(year = lubridate::year(Date),
+         PresAbs = if_else(Density > 0,
+                           1,
+                           0)) %>% 
+  # add cruise number
+  left_join(
+    (cruises_ocean_time_all %>% 
+      dplyr::select(Date, cruise.gen)),
+    by = 'Date')
+
+
+# model construction ------------------------------------------------------
+
+daily_mbm_grid %>% 
+  dplyr::select('bathy', 'topog', 'dist', 'tcur', 'phyto', 'sst', 'temp_sd', 'salt', 'dth') %>% 
+  chart.Correlation()
+
+# On random effect terms:
+# In the context of a linear regression model, a random effect refers to a
+# grouping factor or a categorical variable that introduces additional variation
+# in the response variable, which is not directly modeled as a fixed effect.
+# Random effects account for the variability that arises due to unobserved or
+# unmeasured factors associated with the grouping levels.
+
+## glaucous-winged gull ----------------------------------------------------
+
+
+# testing random effect of year, and zone
+
+
--- code/functions.R
+++ code/functions.R
@@ -1,5 +1,5 @@
 
-# get model weights -------------------------------------------------------
+# get interannual model weights -------------------------------------------------------
 
 # create a function to calculate AIC weights for different models
 getWeightIANN <- 
@@ -86,6 +86,88 @@ getWeightIANN <-
         gam(formula = as.formula(form2),
             family = poisson,
             offset = log(Effort_sqkm),
+            data = data)
+      models$null <- 
+        AIC(m2)
+    }
+    
+    # calculate AIC weights
+    vec_AIC <- unlist(models)
+    dAIC <- vec_AIC - min(vec_AIC)
+    AICw <- exp(-dAIC/2) / sum(exp(-dAIC/2))
+    return(AICw)
+  }
+
+
+# logistic regression weights (marine mammals) ------------------------------------------
+
+get_weight <- 
+  # function will accept inputs where base is a vector of established predictors,
+  # and test will be a vector of predictors to include in the next iteration of forward selection
+  function(base = NULL, test, species = NULL, training = NULL) {
+    if(is.null(training)){
+      stop('NO TRAINING DATA PROVIDED')
+    }
+    if(is.null(species)){
+      warning('SPECIES CODE NOT PROVIDED, TRAINING DATA MAY CONTAIN MULTIPLE SPECIES')
+    }
+    # create a repository
+    models <- vector(
+      mode = 'list',
+      length = length(test)) %>% 
+      set_names(unique(test))
+    
+    data <-
+      training %>% 
+      filter(Species_code == species)
+    
+    # find AIC for each test model
+    if(is.null(base)){
+      # if base is null, find AIC for each test model
+      for(x in names(models)) {
+        form <- 
+          paste('countInt~',
+                x,
+                sep = '')
+        
+        m1 <- 
+          glm(formula = as.formula(form),
+              family = 'binomial',
+              data = data)
+        
+        models[x] <- 
+          AIC(m1)}
+      # if base is null, compare to a model with only a random effect of individual
+      m2 <- 
+        glm(formula = countInt ~ 1,
+            family = 'binomial',
+            data = data)
+      # store the results in the models list
+      models$null <- 
+        AIC(m2)
+    } else{
+      # otherwise, if base is not null, build a new formula
+      # find AIC for each test model
+      for(x in names(models)) {
+        paste('PA~', paste0(base,
+                            collapse = '',
+                            sep = '+'),
+              x,
+              sep = '')
+        
+        m1 <- 
+          glm(formula = as.formula(form),
+              family = 'binomial',
+              data = data)
+        
+        models[x] <- 
+          AIC(m1)}
+      
+      form2 <- 
+        paste('countInt~', paste(base,collapse = '+'))
+      m2 <- 
+        glm(formula = as.formula(form2),
+            family = 'binomial',
             data = data)
       models$null <- 
         AIC(m2)
